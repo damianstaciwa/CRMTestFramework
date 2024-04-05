0e8f60f4c2d038177a85de3738d2036860a5643e
dstaciwa <damian_staciwa@outlook.com>
2024-04-05 15:34:27 +0200

    Project init.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CRMTestFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
