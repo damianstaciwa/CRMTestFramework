affa8f6c6db0e6e910840be4f6804e681ddd85a2
dstaciwa <damian_staciwa@outlook.com>
2024-04-08 15:56:04 +0200

    BasePage.cs added.

---
+++ Base/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using Allure.Net.Commons;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMTestFramework.Base
+{
+    public class BasePage
+    {
+        protected IWebDriver Driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+    }
+}
